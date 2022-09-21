af3b2a29ecaa162b3552dae5420071d42a78eda6
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-09-21 14:23:49 +0300

    10-binary_tree_depth.c

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures depth of a node in binary tree
+ * @tree: input binary tree
+ * Return: depth of tree
+ */
+ 
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t counter = 0;
+
+	if (!tree)
+		return (0);
+
+	while (tree->parent)
+	{
+		counter++;
+		tree = tree->parent;
+	}
+	return (counter);
+}
